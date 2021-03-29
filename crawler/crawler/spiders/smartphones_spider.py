a86ea58e75e77ba0a2d3a4acbe471d549c70112e
Matheus Branco <mbs8@cin.ufpe.br>
2021-03-28 23:48:48 -0300

    Corrects possible crash in getting the price of the product

--- crawler/crawler/spiders/smartphones_spider.py
+++ crawler/crawler/spiders/smartphones_spider.py
@@ -10,6 +10,27 @@ def get_data_from_table(response, info_path, value_path):
 
     return dict(zip(infos, values))
 
+def get_title_product_price(response):
+    title = response.css('title::text').get()
+    if(title is None):
+        title = ""
+    
+    product = response.css('#productTitle::text').get()
+    if(product is not None):
+        product = product.strip()
+    else:
+        product = ""
+
+    price = response.css('#priceblock_ourprice::text').get()
+    if(price is not None):
+        price = price.strip()
+    elif(response.css('#olp_feature_div > div.a-section.a-spacing-small.a-spacing-top-small > span.a-declarative > a > span.a-size-base.a-color-price').get() is not None):
+        price = response.css('#olp_feature_div > div.a-section.a-spacing-small.a-spacing-top-small > span.a-declarative > a > span.a-size-base.a-color-price::text').get()
+    else:
+        price = ""
+        
+    return title, product, price
+        
 class Smartphones(scrapy.Spider):
     name = 'smartphones'
     start_urls = [
@@ -19,9 +40,7 @@ class Smartphones(scrapy.Spider):
     ]
 
     def parse(self, response):
-        title = response.css('title::text').get()
-        product = response.css('#productTitle::text').get().strip()
-        price = response.css('#priceblock_ourprice::text').get().strip()
+        title, product, price = get_title_product_price(response)
         
         # Product information (RAM, Storage, Connection, Weight...)
         product_info = get_data_from_table(response, 
