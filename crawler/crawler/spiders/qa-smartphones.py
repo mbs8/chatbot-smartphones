e2e17d0863d59924cb0ac0ecbd3631ee08e52f4f
Matheus Branco <mbs8@cin.ufpe.br>
2021-03-28 23:47:57 -0300

    Crawls all the pages of QA of the product

--- crawler/crawler/spiders/qa-smartphones.py
+++ crawler/crawler/spiders/qa-smartphones.py
@@ -1,6 +1,30 @@
 import scrapy
 import re
 
+# Return the selected question information in a JSON format
+def extract_question_info(current_question):
+    question = current_question.css('div.a-fixed-left-grid-col.a-col-right > a > span::text').get()
+    if(question is not None):  
+        question = question.strip()
+        question = re.sub(r'<.[A-z]*>|\r|\n', '', question)
+    else:
+        question = ""
+        
+    answer = current_question.css('div.a-fixed-left-grid-col.a-col-right > span').get()
+    if(answer is not None):
+        answer = answer.strip()
+        answer = re.sub(r'<.[A-z]*>|\r|\n', '', answer)
+    else:
+        answer = ""
+
+    q_id = current_question.css('div.a-fixed-left-grid-col.a-col-right > div').attrib['id']
+    if(q_id is not None):
+        q_id = q_id.split('-')[1]
+    else:
+        q_id = ""
+    
+    return {"question" : question, "answer": answer, "q_id": q_id}
+
 # return a list of json in the format:
 # qa: [
 # 		(q1, a1, id1),
@@ -14,38 +38,37 @@ def getQA(response):
     qa_list = []
 
     for selector in selector_qa_list:
-        question = selector.css('div.a-fixed-left-grid-col.a-col-right > a > span::text').get().strip()
-        question = re.sub(r'<.[A-z]*>|\r|\n', '', question)
-
-        answer = selector.css('div.a-fixed-left-grid-col.a-col-right > span').get()
-        if(answer != None):
-            answer = answer.strip()
-            answer = re.sub(r'<.[A-z]*>|\r|\n', '', answer)
-        else:
-            answer = ""
-
-        q_id = selector.css('div.a-fixed-left-grid-col.a-col-right > div').attrib['id'].split('-')[1]
-
-        qa_list.append({"question": question, "answer": answer, "q_id": q_id})
+        question_info = extract_question_info(selector)
+        qa_list.append(question_info)
 
     return qa_list
+
+def get_asin_from_url(url):
+    idx = url.find('asin')
+    asin = url[5+idx:].split('/')[0]
+    return asin
 
 class QaSmartphones(scrapy.Spider):
     name = 'qa-smartphones'
     start_urls = [
         'https://www.amazon.com.br/ask/questions/asin/B08B9C149J',
         'https://www.amazon.com.br/ask/questions/asin/B07XS47PVF',
-        'https://www.amazon.com.br/ask/questions/asin//B08XVV828M'
+        'https://www.amazon.com.br/ask/questions/asin/B08XVV828M'
     ]
 
     def parse(self, response):
-        asin = response.url.split('/')[-1]             # Product identification key in Amazon
+        asin = get_asin_from_url(response.url)         # Product identification key in Amazon
         qa_list = getQA(response)                      # Get the list os questions and answers of the current page
          
         yield {
             'asin': asin,
             'qa': qa_list
         }
+        
+        next_page = response.css('#askPaginationBar > ul > li.a-last > a::attr(href)').get()
+        if(next_page is not None):
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
     
 
         
