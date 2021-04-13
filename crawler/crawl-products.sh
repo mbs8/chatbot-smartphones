16ddc62190b1a57f5f9f66de8a4dd72c7a260b09
Matheus Branco <mbs8@cin.ufpe.br>
2021-04-13 01:01:36 -0300

    Auto rename for products.jl and qa.jl

--- crawler/crawl-products.sh
+++ crawler/crawl-products.sh
@@ -1,2 +1,4 @@
 scrapy crawl --set FEED_EXPORT_ENCODING=utf-8 smartphones -o products.jl
 scrapy crawl --set FEED_EXPORT_ENCODING=utf-8 qa-smartphones -o qa.jl
+mv products.jl products.ndjson
+mv qa.jl qa.ndjson
