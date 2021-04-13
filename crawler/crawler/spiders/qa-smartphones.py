d19b1a7bc4d3b64f5e58128ae693717e03b4088e
Matheus Branco <mbs8@cin.ufpe.br>
2021-04-12 23:26:26 -0300

    Join all the data of th QA in one JSON per product

--- crawler/crawler/spiders/qa-smartphones.py
+++ crawler/crawler/spiders/qa-smartphones.py
@@ -1,74 +1,89 @@
 import scrapy
 import re
 
-# Return the selected question information in a JSON format
-def extract_question_info(current_question):
-    question = current_question.css('div.a-fixed-left-grid-col.a-col-right > a > span::text').get()
-    if(question is not None):  
-        question = question.strip()
-        question = re.sub(r'<.[A-z]*>|\r|\n', '', question)
-    else:
-        question = ""
-        
-    answer = current_question.css('div.a-fixed-left-grid-col.a-col-right > span').get()
-    if(answer is not None):
-        answer = answer.strip()
-        answer = re.sub(r'<.[A-z]*>|\r|\n', '', answer)
-    else:
-        answer = ""
-
-    q_id = current_question.css('div.a-fixed-left-grid-col.a-col-right > div').attrib['id']
-    if(q_id is not None):
-        q_id = q_id.split('-')[1]
-    else:
-        q_id = ""
-    
-    return {"question" : question, "answer": answer, "q_id": q_id}
-
-# return a list of json in the format:
-# qa: [
-# 		(q1, a1, id1),
-# 		(q2, a2, id2),
-# 		...
-# 		(qN, aN, idN)
-# 	]
-def getQA(response):
-    # list containing the pair of questions and answers
-    selector_qa_list = response.xpath('//*[@id="a-page"]/div/div[6]/div/div/div')
-    qa_list = []
-
-    for selector in selector_qa_list:
-        question_info = extract_question_info(selector)
-        qa_list.append(question_info)
-
-    return qa_list
-
-def get_asin_from_url(url):
-    idx = url.find('asin')
-    asin = url[5+idx:].split('/')[0]
-    return asin
-
 class QaSmartphones(scrapy.Spider):
+    question_limit = 50
     name = 'qa-smartphones'
     start_urls = [
         'https://www.amazon.com.br/ask/questions/asin/B08B9C149J',
         'https://www.amazon.com.br/ask/questions/asin/B07XS47PVF',
         'https://www.amazon.com.br/ask/questions/asin/B08XVV828M'
     ]
+    
+    # Return the selected question information in a JSON format
+    def extract_question_info(self, current_question):
+        question = current_question.css('div.a-fixed-left-grid-col.a-col-right > a > span::text').get()
+        if(question is not None):  
+            question = question.strip()
+            question = re.sub(r'<.[A-z]*>|\r|\n', '', question)
+        else:
+            question = ""
+
+        answer = current_question.css('div.a-fixed-left-grid-col.a-col-right > span').get()
+        if(answer is not None):
+            answer = answer.strip()
+            answer = re.sub(r'<.[A-z]*>|\r|\n', '', answer)
+        else:
+            answer = ""
+
+        q_id = current_question.css('div.a-fixed-left-grid-col.a-col-right > div').attrib['id']
+        if(q_id is not None):
+            q_id = q_id.split('-')[1]
+        else:
+            q_id = ""
+
+        return {"question" : question, "answer": answer, "q_id": q_id}
+
+    # return a list of json in the format:
+    # qa: [
+    # 		(q1, a1, id1),
+    # 		(q2, a2, id2),
+    # 		...
+    # 		(qN, aN, idN)
+    # 	]
+    def getQA(self, response, qa_list):
+        # list containing the pair of questions and answers
+        selector_qa_list = response.xpath('//*[@id="a-page"]/div/div[6]/div/div/div')
+        questions = 0
+
+        for selector in selector_qa_list:
+            question_info = self.extract_question_info(selector)
+            qa_list.append(question_info)
+            questions += 1
+        
+        return questions
+
+
+    def get_asin_from_url(self, url):
+        idx = url.find('asin')
+        asin = url[5+idx:].split('/')[0]
+        return asin
 
     def parse(self, response):
-        asin = get_asin_from_url(response.url)         # Product identification key in Amazon
-        qa_list = getQA(response)                      # Get the list os questions and answers of the current page
-         
-        yield {
-            'asin': asin,
-            'qa': qa_list
-        }
+        # Number of questions saved from the product
+        question_count = response.meta.get('question_count', 0)
         
+        # Product identification key in Amazon
+        asin = response.meta.get('asin', self.get_asin_from_url(response.url))  
+        
+        # Get the list os questions and answers of the current page
+        qa_list = response.meta.get('qa', []) 
+        
+        # Get the link to the next QA page
         next_page = response.css('#askPaginationBar > ul > li.a-last > a::attr(href)').get()
-        if(next_page is not None):
+        
+        if(question_count < 50 and next_page is not None):                     
+            question_count += self.getQA(response, qa_list)
+            
+            meta = {'asin': asin, 'qa': qa_list, 'question_count': question_count}
             next_page = response.urljoin(next_page)
-            yield scrapy.Request(next_page, callback=self.parse)
+            yield scrapy.Request(next_page, callback=self.parse, meta=meta)
+                
+        else:
+            yield {
+                'asin': asin,
+                'qa': qa_list
+            }
     
 
         
