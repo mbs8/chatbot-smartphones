fa8b2f82fd068c5fe8e63ba0818470d4d4f078bc
Matheus Branco <matheusbsiqueira96@gmail.com>
2021-06-17 01:57:40 -0300

    Developed actions to request data in Elasticsearch

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -7,21 +7,60 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
+from actions.handles.EsHandle import EsHandle
+import json
+
+
 # class ActionHelloWorld(Action):
-#
+
 #     def name(self) -> Text:
 #         return "action_hello_world"
-#
+
 #     def run(self, dispatcher: CollectingDispatcher,
 #             tracker: Tracker,
 #             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
+
 #         dispatcher.utter_message(text="Hello World!")
-#
+
 #         return []
+
+
+class ActionGetProducts(Action):
+
+    def name(self) -> Text:
+        return "action_list_products"
+    
+    async def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+            handle = EsHandle()
+            products = handle.get_products()
+            
+            products = "\n".join([x[0] for x in products])
+            
+            dispatcher.utter_message(response="utter_list_products", text=products)
+
+            return []
+
+class ActionGetProductInfoByName(Action):
+
+    def name(self) -> Text:
+        return "action_product_info"
+    
+    async def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+            handle = EsHandle()
+            product_name = tracker.get_slot("product")
+            print(product_name)
+            product_info = json.dumps(handle.get_product_by_name(product_name))
+            
+            dispatcher.utter_message(response="utter_list_products", text=product_info)
+
+            return []
---
+++ rasa/actions/handles/EsHandle.py
@@ -0,0 +1,46 @@
+from elasticsearch import Elasticsearch
+
+class EsHandle:
+    def __init__(self):
+        self._products_index = 'products'
+        self._qa_index = 'qa'
+        self._es = Elasticsearch()
+    
+    # Query all the indices that aren't system indices in the elasticsearch
+    def get_useful_indices(self):
+        filtered_idx = filter(lambda x : '.' not in x, [f['index'] for f in self._es.cat.indices(format='json')])
+        indices_names = [idx for idx in filtered_idx]
+        return indices_names
+
+    # Get the product in the 'products' index by name
+    def get_product_by_name(self, name):
+        query_body = {
+        "query": {
+            "match": {
+                "product": name
+            }
+        }
+        }
+        possible_products = []
+        query = self._es.search(index=self._products_index, body=query_body)['hits']['hits']
+        
+        for p in query:
+            possible_products.append(p['_source'])
+        
+        return possible_products
+
+    # Get the products names and id's from the 'products' index
+    def get_products(self):
+        result = []
+        query = self._es.search(index=self._products_index)['hits']['hits']
+        
+        for p in query:
+            p_id = p['_id']
+            p_name = p['_source']['product']
+            result.append((p_name, p_id))
+        return result
+
+if __name__ == "__main__":
+    print("Starting test...")
+    handle = EsHandle()
+    handle.get_useful_indices()
