4748aca68b0debed13e79b9bff816e03d955e506
Matheus Branco <matheusbsiqueira96@gmail.com>
2021-07-01 01:57:53 -0300

    Develops fact consulting of products

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -5,28 +5,13 @@
 # https://rasa.com/docs/rasa/custom-actions
 
 
-# This is a simple example for a custom action which utters "Hello World!"
-
 from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
-from actions.handles.EsHandle import EsHandle
 import json
 
-
-# class ActionHelloWorld(Action):
-
-#     def name(self) -> Text:
-#         return "action_hello_world"
-
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-#         dispatcher.utter_message(text="Hello World!")
-
-#         return []
+from actions.handles.EsHandle import es_handle
 
 
 class ActionGetProducts(Action):
@@ -38,8 +23,7 @@ class ActionGetProducts(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-            handle = EsHandle()
-            products = handle.get_products()
+            products = es_handle.get_products()
             
             products = "\n".join([x[0] for x in products])
             
@@ -56,11 +40,88 @@ class ActionGetProductInfoByName(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-            handle = EsHandle()
             product_name = tracker.get_slot("product")
-            print(product_name)
-            product_info = json.dumps(handle.get_product_by_name(product_name))
+            result = es_handle.get_product_by_name(product_name)
+            if(result == []):
+                product_info = "Infelizmente não possuímos esse produto em estoque."
+            else:
+                product_info = json.dumps(result)
             
             dispatcher.utter_message(response="utter_list_products", text=product_info)
 
-            return []
+            return [SlotSet("product", None)]
+
+class ActionGetProductFact(Action):
+
+    def name(self) -> Text:
+        return "action_product_fact"
+    
+    async def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+            product_name = tracker.get_slot("product")
+            fact = tracker.get_slot("fact")
+            possible_products = es_handle.get_product_fact(product_name, fact)
+            text_to_user = []
+            if(possible_products == []):
+                text_to_user = "Infelizmente não possuímos esse produto em estoque."
+            else:
+                # format output to user
+                for prod in possible_products:
+                    msg = []
+                    for key in prod:
+                        msg.append(f"{key}: {prod[key]}")
+                    msg = " - ".join(msg)
+                    text_to_user.append(msg)
+
+                text_to_user = "\n".join(text_to_user)
+
+            dispatcher.utter_message(response="utter_list_products", text=text_to_user)
+            slots_to_reset = ["product", "fact"]
+            return [SlotSet(slot) for slot in slots_to_reset]
+
+class ActionGetProductPrice(Action):
+
+    def name(self) -> Text:
+        return "action_product_price"
+    
+    async def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+            product_name = tracker.get_slot("product")
+            possible_products = es_handle.get_product_price(product_name)
+
+            if(possible_products == []):
+                msg = "Infelizmente não possuímos esse produto em estoque."
+            else:
+                msg = [" - ".join(prod.values()) for prod in possible_products]
+                msg = "\n".join(msg)
+            
+            dispatcher.utter_message(response="utter_list_products", text=msg)
+            slots_to_reset = ["product"]
+            return [SlotSet(slot) for slot in slots_to_reset]
+
+class ActionCheckStock(Action):
+
+    def name(self) -> Text:
+        return "action_check_stock"
+    
+    async def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+            product_name = tracker.get_slot("product")
+            possible_products = es_handle.get_product_url(product_name)
+
+            if(possible_products == []):
+                msg = "Infelizmente não possuímos esse produto em estoque."
+            else:
+                prods = [" - ".join(prod.values()) for prod in possible_products]
+                prods = "\n".join(prods)
+                msg = f"Nós temos esse(s) produto(s) em estoque:\n{prods}\nAcesse o link para mais detalhes!"
+            
+            dispatcher.utter_message(response="utter_list_products", text=msg)
+            slots_to_reset = ["product"]
+            return [SlotSet(slot) for slot in slots_to_reset]
--- rasa/actions/handles/EsHandle.py
+++ rasa/actions/handles/EsHandle.py
@@ -1,11 +1,12 @@
 from elasticsearch import Elasticsearch
 
+# Singleton class, use variable es_handle
 class EsHandle:
     def __init__(self):
         self._products_index = 'products'
         self._qa_index = 'qa'
         self._es = Elasticsearch()
-    
+
     # Query all the indices that aren't system indices in the elasticsearch
     def get_useful_indices(self):
         filtered_idx = filter(lambda x : '.' not in x, [f['index'] for f in self._es.cat.indices(format='json')])
@@ -39,6 +40,40 @@ class EsHandle:
             p_name = p['_source']['product']
             result.append((p_name, p_id))
         return result
+    
+    # Get specific product fact
+    def get_product_fact(self, product_name, product_fact):
+        products_jsons = self.get_product_by_name(product_name)
+        possible_products = []
+
+        for product in products_jsons:
+            title = product.get("product", "TITLE")
+            fact_json = product["product_info"]
+            value = fact_json.get(product_fact, "FACT")
+            possible_products.append({"Produto": title, product_fact: value})
+        
+        return possible_products
+
+    def get_product_price(self, product_name):
+        products_jsons = self.get_product_by_name(product_name)
+        possible_products = []
+
+        for product in products_jsons:
+            title = product.get("product", "TITLE")
+            price = product.get("price", "PRICE")
+            possible_products.append({"Produto": title, "price": price})
+        
+        return possible_products
+
+    def get_product_url(self, product_name):
+        products_json = self.get_product_by_name(product_name)
+        possible_products = [{"Produto": prod.get("product", "TITLE"), "URL": f"www.amazon.com.br/gp/product/{prod['additional_info']['ASIN']}"} for prod in products_json]
+        return possible_products 
+
+
+
+# Singleton implementation
+es_handle = EsHandle()
 
 if __name__ == "__main__":
     print("Starting test...")
